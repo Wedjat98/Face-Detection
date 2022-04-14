2e35ce8f8c0fe11b99ad32d63bee440b520656f9
狴犴HEIKAN <ame9807@gmail.com>
2022-04-14 12:19:51 +0900

    some Commit

--- 11_edgeDetection_demo.py
+++ 11_edgeDetection_demo.py
@@ -2,17 +2,17 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# 1 读取图像
+# 1 img read
 img = cv.imread('img/sleep cat.jpeg', 0)
-# 2 计算Sobel卷积结果
+# 2 calculate Sobel convolution result
 x = cv.Sobel(img, cv.CV_16S, 1, 0, ksize=-1)  # sobel don't has ksize
 y = cv.Sobel(img, cv.CV_16S, 0, 1, ksize=-1)  # Scharr ksize=-1
-# 3 将数据进行转换
+# 3 data reverse
 Scale_absX = cv.convertScaleAbs(x)  # convert 转换  scale 缩放
 Scale_absY = cv.convertScaleAbs(y)
-# 4 结果合成
+# 4 compose the result
 result = cv.addWeighted(Scale_absX, 0.5, Scale_absY, 0.5, 0)
-# 5 图像显示
+# 5 img show
 plt.figure(figsize=(10, 8), dpi=100)
 plt.subplot(121), plt.imshow(img, cmap=plt.cm.gray), plt.title('origin')
 plt.xticks([]), plt.yticks([])
--- 12_CannyEdgeDetection_demo.py
+++ 12_CannyEdgeDetection_demo.py
@@ -2,13 +2,13 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-# 1 图像读取
+# 1 img read
 img = cv.imread('img/sleep cat.jpeg', 0)
-# 2 Canny边缘检测
+# 2 Canny edge Detection
 lowThreshold = 0
 max_lowThreshold = 100
 canny = cv.Canny(img, lowThreshold, max_lowThreshold)
-# 3 图像展示
+# 3 img show
 plt.figure(figsize=(10, 8), dpi=100)
 plt.subplot(121), plt.imshow(img, cmap=plt.cm.gray), plt.title('origin')
 plt.xticks([]), plt.yticks([])
